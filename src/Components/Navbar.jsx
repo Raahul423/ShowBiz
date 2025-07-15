1986d7c2d61a792e7959c4885d4ce42cb7a74949
Raahul423 <rpal24480@gmail.com>
2025-07-15 18:13:48 +0530

    modify-code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,27 +6,27 @@ import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
 
-  const [isopen ,setIsopen]=useState(false)
+  const [isopen ,setIsopen]=useState(true)
 
 
   const { user } = useUser()
   const { openSignIn } = useClerk()
 
   return (
-    <div className='top-0 left-0  flex justify-around items-center p-6  w-full fixed z-50 md:px-16 lg:px-36'>
+    <div className='top-0 left-0  flex justify-around items-center p-6  w-full fixed z-50   '>
       <Link to={'/'}>
         <img src={assets.logo} alt='Error' className='w-44 bg-blend-color-burn bg-transparent cursor-pointer'  ></img>
       </Link>
 
 
-      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-large z-50 flex-col md:flex-row items-center md:justify-center gap-8 min-md:px-8 py-24 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70  md:bg-white/10  overflow-hidden transition-[width] duration-300 md:border border-gray-300/20  cursor-pointer  flex ${isopen ? 'max-sm:w-full':'max-sm:w-0'}`}>
+      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-large z-50 flex-col md:flex-row items-center md:justify-center gap-8 min-md:px-8 py-3 max-md:py-24 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70  md:bg-white/10  overflow-hidden transition-[width] duration-300 md:border border-gray-300/20  cursor-pointer  flex ${isopen ? 'max-sm:w-full':'max-sm:w-0'}`}>
 
         <X onClick={()=>setIsopen(!isopen)} className='sm:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' />
 
         <Link onClick={()=>{scrollTo(0,0), setIsopen(!isopen) }} to='/'>Home</Link>
         <Link onClick={()=>{scrollTo(0,0), setIsopen(!isopen) }} to='/Movies'>Movies</Link>
         <Link onClick={()=>{scrollTo(0,0), setIsopen(!isopen) }} to='/Theaters'>Theaters</Link>
-        <Link onClick={()=>{scrollTo(0,0), setIsopen(!isopen) }} to='/Favorite'>Favourite</Link>
+        <Link to='/Favorite'>Favourite</Link>
 
       </div>
 
