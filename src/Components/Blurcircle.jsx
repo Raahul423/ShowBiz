9f7aa55f881878d3102dc8a8d5d2d330861d99bd
raahul423 <rpal24480@gmail.com>
2025-07-29 13:18:19 +0530

    recovery project

--- src/Components/Blurcircle.jsx
+++ src/Components/Blurcircle.jsx
@@ -2,7 +2,7 @@
 const Blurcircle = ({top,bottom,left,right}) => {
   return (
     <div className="absolute -z-10 h-75 w-75 rounded-full bg-[#40161e]  blur-3xl  " style={{top:top,buttom:bottom,left:left,right:right}}>
-    </div>
+    </div> // This component is blue circle 
   )
 }
 
